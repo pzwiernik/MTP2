123466345b58af52f4e44408fdbb24d4a790f9d1
Piotr Zwiernik <piotr.zwiernik@gmail.com>
2020-06-29 12:40:10 +0200

    The dual MLE function is completely rewritten like other block-coordinate descent algorithms.

--- R/LADualMLE.R
+++ R/LADualMLE.R
@@ -1,4 +1,4 @@
-#' A coordinate descent algorithm to find the dual maximum likelihood estimator for associated Gaussian distributions
+#' A coordinate descent algorithm to find the dual maximum likelihood estimator for locally associated Gaussian distributions
 #'
 #' This function implements a simple coordinate descent algorithm to find the maximum likelihood estimator over
 #' Gaussian MTP2 distributions. For details see Lauritzen, Uhler, Zwiernik (2017).
@@ -13,42 +13,31 @@
 #' @examples
 #' 
 #' 
-LADualMLE <- function(W,edges=combn(1:nrow(W),2,simplify=FALSE), tol=1e-8){
-  p <- ncol(W)
-  row.names(W) <- colnames(W) <- as.character(1:p)
-  Sig <- W
-  Sig0 <- 2*Sig
+LADualMLE <- function(W, tol=1e-8){
+  AM <- (abs(W)>1e-8)
+  K <- W
+  d <- nrow(K)
   it <- 0
-  if (p==2){
-    return(list(Sig=solve(matrix(c(W[1,1],min(c(W[1,2],0)),min(c(W[1,2],0)),W[2,2]),2,2))))
+  cat("** The algorithm maximizes the dual log-likelihood with edge positivity constraints.\n\n")
+  cat("* To establish convergence we track the  KKT conditions. Dual feasibility holds at each step.\n")
+  cat("  All edge covariances must be >=",-tol,"\b.    Complementary slackness in each entry <=",tol,"\b.\n")
+  cat("Primal feasibility | Complementary slackness\n")
+  Shat <- solve(K)
+  zeros <- list()
+  nonzeros <- list()
+  for (j in 1:d){
+    zeros[[j]] <- setdiff(which(abs(W[j,])<1e-8),j)
+    nonzeros[[j]] <- setdiff(which(abs(W[j,])>=1e-8),j)
   }
-  while (sum(abs(Sig-Sig0))>tol){
-    Sig0 <- Sig
-    it <- it+1
-    for (A in edges){
-      i <- A[1]
-      j <- A[2]
-      B <- setdiff(1:p,A)
-      L <- Sig[A,B]%*%solve(Sig[B,B])%*%Sig[B,A]
-      if (L[1,2]>=W[i,j]/(W[i,i]*W[j,j]-W[i,j]^2)) Sig[A,A] <- solve(W[A,A])+L
-      else {
-        au <- (1+sqrt(1+4*L[1,2]^2*W[i,i]*W[j,j]))/2
-        Sig[A,A] <- diag(c(L[1,1]+au/W[i,i],L[2,2]+au/W[j,j]))
-      }
+  while (min(Shat*AM)< -tol || max(abs(Shat*(W-K)))>tol){
+    for (j in 1:d){
+      A <- rbind(diag(d)[zeros[[j]],-j],-diag(d)[nonzeros[[j]],-j])
+      b <- c(W[j,zeros[[j]]],-W[j,nonzeros[[j]]])
+      y <- solve.QP(Dmat=solve(K[-j,-j]),dvec=rep(0,d-1),Amat=t(A),bvec=b,meq=length(zeros[[j]]))$solution
+      K[j,-j] <- K[-j,j] <- y
     }
-    # for (i in 1:(p-1)){
-    #   for (j in (i+1):p){
-    #     A <- union(i,j)
-    #     B <- setdiff(1:p,A)
-    #     L <- Sig[A,B]%*%solve(Sig[B,B])%*%Sig[B,A]
-    #     if (L[1,2]>=W[i,j]/(W[i,i]*W[j,j]-W[i,j]^2)) Sig[A,A] <- solve(W[A,A])+L
-    #     else {
-    #       au <- (1+sqrt(1+4*L[1,2]^2*W[i,i]*W[j,j]))/2
-    #       Sig[A,A] <- diag(c(L[1,1]+au/W[i,i],L[2,2]+au/W[j,j]))
-    #     }
-    #   }
-    # }
+    Shat  <- solve(K)
+    cat(min(Shat*AM),"       |",max(abs(Shat*(W-K))),"\n")
   }
-  # output
-  return(list(Sig=Sig,it))
+  return(list(Sig=Shat,it))
 }
